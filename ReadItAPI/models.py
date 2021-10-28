06addb1d56e496420dcdd1c8e95e1b789098fbce
gabriel <gabriel.filiot@epsi.fr>
2021-10-28 11:44:46 +0200

    migrate

--- ReadItAPI/models.py
+++ ReadItAPI/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
-# Create your models here.
+class Communauté(models.Model):
+    nom = models.CharField(max_length=200, unique=True)
+    adherent = models.IntegerField(null=True)
+    cree_le = models.DateTimeField(auto_now_add=True)
