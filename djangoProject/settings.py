e88d95eb751047c9aa21a7fb8a39f122b7c6618d
gabgabb <70889534+gabgabb@users.noreply.github.com>
2021-11-16 20:30:06 +0100

    connexion bdd externe

--- djangoProject/settings.py
+++ djangoProject/settings.py
@@ -77,11 +77,14 @@ WSGI_APPLICATION = 'djangoProject.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+              'ENGINE': 'django.db.backends.mysql',
+              'NAME': 'PythonAPI',
+              'USER': 'admin',
+              'PASSWORD': 'Connexionbdd2021',
+              'HOST': 'pythonapi.cmigckpo8ot2.eu-west-3.rds.amazonaws.com',
+              'PORT': '3306'
+            }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
